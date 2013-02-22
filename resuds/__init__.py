dabb02300cd2b4303c844e05a41c2a43ef823321
Tommaso Allevi <tomallevi@gmail.com>
2013-02-22 15:41:15 +0100

    Change version in __init__ file

--- resuds/__init__.py
+++ resuds/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Tommaso Allevi <team@appsfuel.com>'
-__version__ = '0.1'
+__version__ = '0.2'
 __classifiers__ = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
